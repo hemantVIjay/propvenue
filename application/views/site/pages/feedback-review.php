b17bbd7c3c2f0308bce709fb78475c5b71b9fb8d
Prem Singh <61229245+singhprem166@users.noreply.github.com>
2022-04-21 21:41:10 +0530

    dsn

---
+++ application/views/site/pages/feedback-review.php
@@ -0,0 +1,131 @@
+<div class="fdbksec py-5">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-xl-11">
+				<h2 class="pg-title-big text-center">Rating & Reviews</h2>
+				<div class="row">
+					<div class="col-md-6 mb-3">
+						<div class="card">
+							<div class="card-body">
+								<div class="d-flex justify-content-between align-items-center mb-3">
+									<div class="d-flex">
+										<span class="rwsnbg text-center rounded-circle fw-bold fs-5">PK</span>
+										<div>
+											<div class="rwnm fw-bold fs-6">Pradeep Kumar</div>
+											<div class="rwdt text-gray">21-Apr-2022</div>
+										</div>
+									</div>
+									<div class="rwcb mb-2">
+										<span class="badge bg-success fw-bold me-2">3.5/5</span>
+										<span class="strrw"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-half"></i><i class="bi bi-star"></i></span>
+									</div>
+								</div>
+								<p class="mb-0">Nice builder floor apartment. I have very good experience with the builder, good communication, good service, and nice people, flats are nice and basic facilities, metro connectivity is also there.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 mb-3">
+						<div class="card">
+							<div class="card-body">
+								<div class="d-flex justify-content-between align-items-center mb-3">
+									<div class="d-flex">
+										<span class="rwsnbg text-center rounded-circle fw-bold fs-5">PK</span>
+										<div>
+											<div class="rwnm fw-bold fs-6">Pradeep Kumar</div>
+											<div class="rwdt text-gray">21-Apr-2022</div>
+										</div>
+									</div>
+									<div class="rwcb mb-2">
+										<span class="badge bg-success fw-bold me-2">3.5/5</span>
+										<span class="strrw"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-half"></i><i class="bi bi-star"></i></span>
+									</div>
+								</div>
+								<p class="mb-0">Nice builder floor apartment. I have very good experience with the builder, good communication, good service, and nice people, flats are nice and basic facilities, metro connectivity is also there.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 mb-3">
+						<div class="card">
+							<div class="card-body">
+								<div class="d-flex justify-content-between align-items-center mb-3">
+									<div class="d-flex">
+										<span class="rwsnbg text-center rounded-circle fw-bold fs-5">PK</span>
+										<div>
+											<div class="rwnm fw-bold fs-6">Pradeep Kumar</div>
+											<div class="rwdt text-gray">21-Apr-2022</div>
+										</div>
+									</div>
+									<div class="rwcb mb-2">
+										<span class="badge bg-success fw-bold me-2">3.5/5</span>
+										<span class="strrw"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-half"></i><i class="bi bi-star"></i></span>
+									</div>
+								</div>
+								<p class="mb-0">Nice builder floor apartment. I have very good experience with the builder, good communication, good service, and nice people, flats are nice and basic facilities, metro connectivity is also there.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 mb-3">
+						<div class="card">
+							<div class="card-body">
+								<div class="d-flex justify-content-between align-items-center mb-3">
+									<div class="d-flex">
+										<span class="rwsnbg text-center rounded-circle fw-bold fs-5">PK</span>
+										<div>
+											<div class="rwnm fw-bold fs-6">Pradeep Kumar</div>
+											<div class="rwdt text-gray">21-Apr-2022</div>
+										</div>
+									</div>
+									<div class="rwcb mb-2">
+										<span class="badge bg-success fw-bold me-2">3.5/5</span>
+										<span class="strrw"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-half"></i><i class="bi bi-star"></i></span>
+									</div>
+								</div>
+								<p class="mb-0">Nice builder floor apartment. I have very good experience with the builder, good communication, good service, and nice people, flats are nice and basic facilities, metro connectivity is also there.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 mb-3">
+						<div class="card">
+							<div class="card-body">
+								<div class="d-flex justify-content-between align-items-center mb-3">
+									<div class="d-flex">
+										<span class="rwsnbg text-center rounded-circle fw-bold fs-5">PK</span>
+										<div>
+											<div class="rwnm fw-bold fs-6">Pradeep Kumar</div>
+											<div class="rwdt text-gray">21-Apr-2022</div>
+										</div>
+									</div>
+									<div class="rwcb mb-2">
+										<span class="badge bg-success fw-bold me-2">3.5/5</span>
+										<span class="strrw"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-half"></i><i class="bi bi-star"></i></span>
+									</div>
+								</div>
+								<p class="mb-0">Nice builder floor apartment. I have very good experience with the builder, good communication, good service, and nice people, flats are nice and basic facilities, metro connectivity is also there.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6 mb-3">
+						<div class="card">
+							<div class="card-body">
+								<div class="d-flex justify-content-between align-items-center mb-3">
+									<div class="d-flex">
+										<span class="rwsnbg text-center rounded-circle fw-bold fs-5">PK</span>
+										<div>
+											<div class="rwnm fw-bold fs-6">Pradeep Kumar</div>
+											<div class="rwdt text-gray">21-Apr-2022</div>
+										</div>
+									</div>
+									<div class="rwcb mb-2">
+										<span class="badge bg-success fw-bold me-2">3.5/5</span>
+										<span class="strrw"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-half"></i><i class="bi bi-star"></i></span>
+									</div>
+								</div>
+								<p class="mb-0">Nice builder floor apartment. I have very good experience with the builder, good communication, good service, and nice people, flats are nice and basic facilities, metro connectivity is also there.</p>
+							</div>
+						</div>
+					</div>
+				</div>				
+			</div>
+		</div>
+	</div>
+</div>
